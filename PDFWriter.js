00e4897be7ba0b61ec88b3ae38304ccd49f8a55d
Lucas Ferreira <lucas-ferreira@outlook.com>
2022-02-13 21:51:53 -0300

    ExcelConverter finalizado

---
+++ PDFWriter.js
@@ -0,0 +1,12 @@
+const pdf = require("html-pdf");
+
+class PDFWriter {
+
+    static WritePDF(filename, html){
+
+        pdf.create(html,{}).toFile(filename, (err) =>{});
+
+    }
+
+};
+module.exports = PDFWriter;
